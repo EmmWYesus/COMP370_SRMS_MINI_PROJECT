755529a14ff7200bb247aa060223d010ed723ba7
EmmWYesus <Emnet.Weldeyesus@student.ufv.ca>
2025-10-15 01:40:09 -0700

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,25 +1,24 @@
 public class Server {
     protected String name;
     protected boolean isActive;
-
     public Server(String name) {
         this.name = name;
         this.isActive = false;
     }
     public void activate() {
         isActive = true;
-        System.out.println(name + " is now active.");
+        System.out.println(name + " has been activated and is now ready to handle requests.");
     }
     public void deactivate() {
         isActive = false;
-        System.out.println(name + " is now inactive.");
+        System.out.println(name + " has been deactivated and will no longer process requests.");
     }
     public void processRequest(String request) {
         if (isActive) {
             System.out.println(name + " processed request: " + request);
         } 
         else {
-            System.out.println(name + " cannot process request (inactive).");
+            System.out.println(name + " is inactive and cannot process the request.");
         }
     }
 }
