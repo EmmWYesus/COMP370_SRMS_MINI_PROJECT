76def25c0bb9a5026cdbc50a757cf92c618f64c6
EmmWYesus <Emnet.Weldeyesus@student.ufv.ca>
2025-11-25 19:27:26 -0800

    Update Monitor.java

--- src/mproject2/Monitor.java
+++ src/mproject2/Monitor.java
@@ -2,35 +2,23 @@ package mproject2;
 
 public class Monitor implements Observer {
     private static Monitor instance;
-    private PrimaryServer primary;
-    private BackupServer backup;
 
-    // Private constructor (Singleton)
-    private Monitor() {}
+    private Monitor() {}  // Private so only getInstance() can create
 
-    // Global access (Singleton)
     public static synchronized Monitor getInstance() {
-        if (instance == null) instance = new Monitor();
+        if (instance == null) {
+            instance = new Monitor();
+        }
         return instance;
-    }
-
-    // System setup (called once)
-    public void configure(PrimaryServer p, BackupServer b) {
-        this.primary = p;
-        this.backup = b;
     }
 
     @Override
     public void update(String message) {
-        System.out.println("[Monitor] Event received: " + message);
-        if (message.contains("Primary Server is now INACTIVE")) {
-            System.out.println("[Monitor] Switching to backup...");
-            backup.promoteToPrimary();
-            primary.deactivate(); // Ensure consistency
-        }
+        System.out.println("Monitor received event: " + message);
+        // Here you can later add failover logic (switch to backup)
     }
 
-    public ServerType getActiveServer() {
-        return (primary != null && primary.isActive()) ? primary : backup;
+    public void checkServer(ServerType server) {
+        System.out.println("Checking status of server: " + server.getName());
     }
 }
