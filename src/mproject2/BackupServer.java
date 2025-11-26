8f161be6dc4eeba488c9b9bd775f079cded92cf2
EmmWYesus <Emnet.Weldeyesus@student.ufv.ca>
2025-11-25 19:32:55 -0800

    Update BackupServer.java

--- src/mproject2/BackupServer.java
+++ src/mproject2/BackupServer.java
@@ -1,21 +1,12 @@
 package mproject2;
 
 public class BackupServer extends ServerType {
-    public BackupServer() {
-        super("Backup Server");
+    public BackupServer(String host, int port) {
+        super(host, port, "Backup Server");
     }
 
     @Override
-    public void handleRequest(String request) {
-        if (isActive) {
-            System.out.println(name + " processed request: " + request);
-        } else {
-            System.out.println(name + " cannot process request (inactive).");
-        }
-    }
-
-    public void promoteToPrimary() {
-        System.out.println(name + " promoted to Primary!");
-        activate();
+    public void handleRequest() {
+        System.out.println(name + " handling client request.");
     }
 }
